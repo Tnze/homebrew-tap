7c46ae9964cbfaa3cfa348a4cca7a18070f20e22
Tnze <cjd001113@outlook.com>
2019-06-19 18:29:25 +0800

    support "§" format

--- mcping.rb
+++ mcping.rb
@@ -4,8 +4,8 @@
 class Mcping < Formula
   desc "a tool for pinging minecraft server"
   homepage "https://github.com/Tnze/go-mc/tree/master/cmd/mcping"
-  url "https://github.com/Tnze/go-mc/releases/download/v1.0.0/mcping"
-  sha256 "24065e328c6964c6c3eef78dfb31e1fdfe9db492c1dd7486b96681c9aedfb0b8"
+  url "https://github.com/Tnze/go-mc/releases/download/v1.0.1/mcping"
+  sha256 "61ff0aaee7a63f4ed540565c5097e8d0e45e9ab02fc7c663f2465e0bebeea385"
   # depends_on "cmake" => :build
 
   def install
